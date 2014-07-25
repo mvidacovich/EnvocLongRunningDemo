500d6a9b3041d17b2c79b12a2e4917f1125e5569
mvid <mvid@envoc.com>
2014-07-25 10:00:26 -0500

    Forgot to pass id

--- Envoc.AzureLongRunningTask.Web/Scripts/controllers/processImageCtrl.js
+++ Envoc.AzureLongRunningTask.Web/Scripts/controllers/processImageCtrl.js
@@ -36,8 +36,14 @@
 
         };
 
+        $scope.uploader.bind('BeforeUpload', function (up, file) {
+            // Give server the id for reference
+            up.settings.url = cleanUrl + "?id=" + file.id;
+        });
+
         $scope.uploader = new plupload.Uploader(settings);
         $scope.uploader.init();
+
 
         $scope.uploader.bind('FilesAdded', function (up, files) {
             $scope.uploader.start();
